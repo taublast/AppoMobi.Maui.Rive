486a6d2c744d804df7b9e75852dbdf4dece04b11
Nick Kovalsky <nickyboom@mail.ru>
2023-06-25 18:26:17 +0300

    initial no refs

---
+++ src/AppoMobi.Maui.Rive/Platforms/Tizen/PlatformClass1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AppoMobi.Maui.Rive
+{
+	// All the code in this file is only included on Tizen.
+	public class PlatformClass1
+	{
+	}
+}
---
+++ src/AppoMobi.Maui.Rive/Rive.cs
@@ -0,0 +1,27 @@
+﻿using RiveSharp;
+using SkiaSharp;
+
+namespace AppoMobi.Maui.Rive
+{
+	// the main todo remains to create universal code, meanwhile will use models from RiveSharp 
+	public class Rive
+	{
+		class WindowsRenderer : Renderer
+		{
+			public WindowsRenderer(SKCanvas skCanvas) : base(skCanvas)
+			{
+			}
+
+			public static void Init()
+			{
+			}
+		}
+
+		public static void Init()
+		{
+			// windows
+			WindowsRenderer.Init();
+
+		}
+	}
+}
